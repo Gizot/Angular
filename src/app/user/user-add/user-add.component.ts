0bf9722a749ca3f6f0fb3e386e61919212d50a7f
Gizot <tastangizem@gmail.com>
2023-07-03 18:03:12 +0300

    fix bugs & user-add cancel function

--- src/app/user/user-add/user-add.component.ts
+++ src/app/user/user-add/user-add.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from '../user';
 import { UserService } from '../user.service';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-user-add',
   templateUrl: './user-add.component.html',
@@ -17,18 +18,29 @@ export class UserAddComponent {
     isActive: false
   }
 
-  constructor(private userService: UserService) {
+  constructor(private userService: UserService, private router: Router) {
     this.userService.setUsers();
 
   }
-  //userService componentinden veri önce setUsers fonksiyonunu kullanarak verileri çektik,
+  //userService componentinden veri önce setUsers fonksiyonunu kullanarak verileri çekiyor,
   //sonra getUsers kullanarak userCount fonksiyonu ile son userId'yi bularak yeni userId
-  //atamaları gerçekleştirildi.
+  //atamalarını gerçekleştirdik.
   handleCreateClick() {
     this.user.userId = this.userService.getUsers()[this.userService.userCount() -1 ].userId + 1; 
+    if(this.user.username==="" || this.user.email === "" || this.user.creationDate === "")
+    alert("Fill the empty spaces.")
+    else if(this.userService.checkUnique(this.user.username, this.user.email, this.user.userId)===false)
+    alert("This user already exist.")
+    else {
+      this.userService.addUser(this.user);
+      console.log(this.userService.getUsers());
+    }
+
     this.userService.addUser(this.user);
     console.log(this.userService.getUsers());
+  }
 
-    
+  handleCancelClick(){
+    this.router.navigateByUrl("/userlist");
   }
 }
